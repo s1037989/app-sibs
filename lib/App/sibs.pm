eae68feb7fb7cb3e6d1ffa173266669ef545d56e
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-10-26 16:34:55 +0200

    Released version 0.11
           * Fix $ENV{HOME} in unittests

--- lib/App/sibs.pm
+++ lib/App/sibs.pm
@@ -6,7 +6,7 @@ App::sibs - Simple incremental backup system
 
 =head1 VERSION
 
-0.10
+0.11
 
 =head1 DESCRIPTION
 
@@ -82,7 +82,7 @@ Default C<exclude> may change in future release.
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 COPYRIGHT
 
